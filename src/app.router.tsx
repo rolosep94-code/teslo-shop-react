c0c0c03a75780b820dba3dba366900584e064820
Rolando <rolosep94@hotmail.com>
2026-01-28 04:57:20 -0300

    add export const appRouter

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,5 +1,5 @@
 import { lazy } from "react";
-import { createBrowserRouter, Navigate } from "react-router";
+import { createBrowserRouter, createHashRouter, Navigate } from "react-router";
 
 import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
@@ -19,7 +19,8 @@ const AuthLayout = lazy(() => import('./auth/layouts/AuthLayout'));
 const AdminLayout = lazy(() => import('./admin/layouts/AdminLayout'));
 
 
-export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
+// export const appRouter = createBrowserRouter([  
     // Main routes
     {
         path: '/',
